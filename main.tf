79b70ada71130b101a7948e5ccf0f196afb7a506
Sven Christ <schrist2@smail.uni-koeln.de>
2019-12-21 21:50:50 +0100

    Added VPC and a default subnet.

--- main.tf
+++ main.tf
@@ -4,6 +4,27 @@ provider "aws" {
   secret_key = file(var.aws_secret_key_file)
 }
 
+resource "aws_vpc" "default" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_subnet" "default" {
+  vpc_id = aws_vpc.default.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
 resource "aws_key_pair" "auth" {
   key_name = var.aws_key_pair_name
   public_key = file(var.public_key_file)
@@ -13,5 +34,6 @@ resource "aws_instance" "web" {
   ami = var.aws_ec2_ami
   instance_type = var.aws_ec2_type
   key_name = aws_key_pair.auth.id
-  associate_public_ip_address = "true"
+  subnet_id = aws_subnet.default.id
+  associate_public_ip_address = true
 }

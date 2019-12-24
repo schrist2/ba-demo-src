d09b0680fca7c8129d440267377b152a7b0809bb
Sven Christ <schrist2@smail.uni-koeln.de>
2019-12-24 13:37:59 +0100

    Added DB subnet group and second subnet + AZ.

--- main.tf
+++ main.tf
@@ -13,10 +13,21 @@ resource "aws_internet_gateway" "default" {
   vpc_id = aws_vpc.default.id
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "default" {
   vpc_id = aws_vpc.default.id
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id = aws_vpc.default.id
+  cidr_block = "10.1.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
 resource "aws_route" "internet_access" {
@@ -116,19 +127,25 @@ resource "aws_instance" "web" {
   #user_data = "/bin/bash ~/provision/init.sh"
 }
 
-resource "aws_s3_bucket" "web_static" {
-  bucket = "static_content"
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "static-files"
   acl = "public-read"
 }
 
+resource "aws_db_subnet_group" "default" {
+  name = "default_db_subnet_group"
+  subnet_ids = [aws_subnet.default.id, aws_subnet.subnet_2.id]
+}
+
 resource "aws_db_instance" "db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mysql"
-  username             = "root"
-  password             = "12345"
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2.micro"
+  name = "mysql"
+  username = "root"
+  password = "12345"
   parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = aws_db_subnet_group.default.name
 }

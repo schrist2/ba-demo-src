6d462f4e9a4a965b52a5b226f036fd9336b255d8
Sven Christ <schrist2@smail.uni-koeln.de>
2019-12-24 13:26:40 +0100

    Added MySQL DB instance.

--- main.tf
+++ main.tf
@@ -116,7 +116,19 @@ resource "aws_instance" "web" {
   #user_data = "/bin/bash ~/provision/init.sh"
 }
 
-resource "aws_s3_bucket" "static" {
-  bucket = "static"
+resource "aws_s3_bucket" "web_static" {
+  bucket = "static_content"
   acl = "public-read"
+}
+
+resource "aws_db_instance" "db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mysql"
+  username             = "root"
+  password             = "12345"
+  parameter_group_name = "default.mysql5.7"
 }
